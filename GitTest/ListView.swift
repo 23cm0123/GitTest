0a01bc87d3f1a9cca9f713e042d9bd58d02235fd
Kristen23 <137244397+23cm0123@users.noreply.github.com>
2024-09-01 20:45:03 +0900

    Add ListView

---
+++ GitTest/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  GitTest
+//
+//  Created by Kristen on 2024/09/01.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
