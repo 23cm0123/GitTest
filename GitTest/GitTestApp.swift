745ee46707a0a8c5f118a9889b3f8f07b42518aa
Kristen23 <137244397+23cm0123@users.noreply.github.com>
2024-09-01 19:44:11 +0900

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Kristen on 2024/09/01.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
