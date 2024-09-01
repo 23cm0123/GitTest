6b3cedf213e963177ec1cd493d2f1d7a822a4ffd
Kristen23 <137244397+23cm0123@users.noreply.github.com>
2024-09-01 21:11:14 +0900

    Add Button, dropDownList

--- GitTest/ContentView.swift
+++ GitTest/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isRotate = false
+    @State var isShowList = false
+    
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "heart.fill")
                 .imageScale(.large)
-                .foregroundStyle(.tint)
+                .foregroundStyle(.pink)
+            
             Text("Git Testing Now...")
-        }
-        .padding()
+                .padding(.bottom)
+            
+            Button(action: {
+                isRotate.toggle()
+                isShowList.toggle()
+            }, label: {
+                HStack{
+                    Text("ShowList")
+                    Image(systemName: "triangle.fill")
+                        .rotationEffect(isRotate ? .zero : .degrees(180))
+                }
+                .padding(8)
+                .background(.gray.opacity(0.2))
+                .clipShape(Capsule())
+            }) // Button
+            
+            if isShowList {
+                ListView()
+                    .frame(width: 300, height: 300)
+            } else {
+                Rectangle()
+                    .fill(.clear)
+                    .frame(width: 300, height: 300)
+            }
+            
+        } // VStack
         .padding()
     }
 }
--- GitTest/ListView.swift
+++ GitTest/ListView.swift
@@ -8,8 +8,16 @@
 import SwiftUI
 
 struct ListView: View {
+    
+    var testData = ["a", "b", "c", "d", "e"]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(testData, id: \.self){ alp in
+                Text(alp)
+            }
+        }
     }
 }
 
